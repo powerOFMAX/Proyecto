5d1a4d457677dec9ae0ca9c20f2965668d3e12cc
Maxi <maxi.v@bravenew.tech>
2018-10-02 09:55:03 -0300

    footer at the bottom / value of pagination elements

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
     {   
         try
         {
-            $post = Post::orderBy("created_at",'desc')->paginate(5);
+            $post = Post::orderBy("created_at",'desc')->paginate(10);
             return response()->json($post);
         }
         catch(\Exception $e)
